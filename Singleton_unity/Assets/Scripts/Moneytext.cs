dc34939199c606cc956160022f37258c73ad552a
WislonLin <p96091123@gs.ncku.edu.tw>
2021-03-19 18:18:49 +0800

    Version_01

---
+++ Singleton_unity/Assets/Scripts/Moneytext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Moneytext   /*不能+MonoBehaviour*/
+{
+    public Text moneytext;
+    public int moneytextmoney;
+    public void MoneyFucntion(int moneys)
+    {
+        moneytextmoney = moneys;
+        moneytext.text = "money" + moneytextmoney.ToString();
+    }
+    public void Initial(Text _MoneyText )
+    {
+        moneytext = _MoneyText;
+    }
+
+}
---
+++ Singleton_unity/Assets/Scripts/Scoretext.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Scoretext   /*不能+MonoBehaviour*/
+{
+    public Text scoretext;
+    public int Scoretextscores;
+    private Button Add, Reduce;
+    private Scoretext()
+    {
+    }
+    private static Scoretext m_Scoretext;
+    public static Scoretext GetScoretext  /*取GetScoretext*/
+    {
+        get
+        {
+            if(m_Scoretext == null)
+            {
+                m_Scoretext = new Scoretext();
+            }
+            return m_Scoretext;
+        }
+    }
+    public void ScoreFucntion(int scores)
+    {
+        Scoretextscores = scores;
+        scoretext.text = "score"+Scoretextscores.ToString();
+    }
+
+    public void Initial(Text _ScoreText, Button Adds , Button Reduces )
+    {
+        scoretext = _ScoreText;
+        Add = Adds;
+        Reduce = Reduces;
+    }
+}
