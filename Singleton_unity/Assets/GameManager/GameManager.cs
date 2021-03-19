35b4dd3973b98d2fa491ec65ef6488644c56bf35
WislonLin <p96091123@gs.ncku.edu.tw>
2021-03-20 00:30:07 +0800

    Revert "Revert "Version_01""
    This reverts commit cd052f8dafddc2a3272e1364236d7d39375ea0ca.

---
+++ Singleton_unity/Assets/GameManager/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public Text scoretext;
+    private Scoretext _scoretext = Scoretext.GetScoretext;/*調用Scoretext之變數*/
+
+    [SerializeField]
+    private int score;
+
+    public Button Add, Reduce;
+    /*------------------------*/
+    public Text moneytext;
+	private static Moneytext _moneytext = new Moneytext();/*可以使用Money*/
+
+    [SerializeField]
+    private int money;
+    /*------------------------*/
+    void Awake()
+    {
+        _scoretext.Initial(scoretext, Add, Reduce);
+        _scoretext.ScoreFucntion(score);
+       /*-----------------------------------------*/
+        _moneytext.Initial(moneytext);
+        _moneytext.MoneyFucntion(money);
+
+    }
+    public void AddScore()
+    {
+         score++;
+        _scoretext.ScoreFucntion(score);
+    }
+    public void ReduceScore()
+    {
+         score--;
+        _scoretext.ScoreFucntion(score);
+    }
+}
